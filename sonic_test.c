0b029a73f8eb5c1c818c2c273cd108ff27e6081b
ming.kong <ming.kong@zoom.us>
2025-03-07 18:42:04 +0800

    test cpu percentage

--- sonic_test.c
+++ sonic_test.c
@@ -3,11 +3,36 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#if defined(_WIN32)
+#include <windows.h>    /* timer */
+#else   
+#include <sys/time.h>
+#endif
+
 #include "sonic.h"
 
 #define SAMPLE_RATE	48000
 #define CHANNEL_NUM 2
 #define SPEED 2
+
+unsigned long GetHighResolutionTime(void) /* O  time in usec*/
+{
+#if defined(_WIN32)
+    /* Returns a time counter in microsec   */
+    /* the resolution is platform dependent */
+    /* but is typically 1.62 us resolution  */
+    LARGE_INTEGER lpPerformanceCount;
+    LARGE_INTEGER lpFrequency;
+    QueryPerformanceCounter(&lpPerformanceCount);
+    QueryPerformanceFrequency(&lpFrequency);
+    return (unsigned long)((1000000*(lpPerformanceCount.QuadPart)) / lpFrequency.QuadPart);
+#else
+    /* Linux or Mac*/
+    struct timeval tv;
+    gettimeofday(&tv, 0);
+    return((tv.tv_sec*1000000)+(tv.tv_usec));
+#endif
+}
 
 int main()
 {
@@ -38,10 +63,15 @@ int main()
 	int nSamplesTotalIn = 0;
 	int nSamplesTotalOut = 0;
 
+	unsigned long startTime, totalTime = 0;
+
 	do
 	{
 		nSamplesIn = fread(pInBuf, 2*CHANNEL_NUM, SAMPLE_RATE/100, pFileIn);
 		nSamplesTotalIn += nSamplesIn;
+
+		startTime = GetHighResolutionTime();
+
 		if(nSamplesIn == 0)
 		{
 			sonicFlushStream(stream);
@@ -54,13 +84,19 @@ int main()
 		{
 			nSamplesOut = sonicReadShortFromStream(stream, pOutBuf, SAMPLE_RATE/100);
 			nSamplesTotalOut += nSamplesOut;
+			/*
 			if(nSamplesOut > 0)
 			{
 				fwrite(pOutBuf, 2*CHANNEL_NUM, nSamplesOut, pFileOut);
 			}
+				*/
 		} while (nSamplesOut > 0);
+
+		totalTime += GetHighResolutionTime() - startTime;
 		
 	} while (nSamplesIn > 0);
+
+	printf("total time: %d\n", totalTime);
 
 	sonicDestroyStream(stream);
 	fclose(pFileIn);
